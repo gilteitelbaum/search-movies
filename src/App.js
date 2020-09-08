f9e2f9222cab727a14e29d7258821ebbaa937222
Gil Teitelbaum <gil@nationalparalegal.edu>
2020-09-08 20:56:07 +0300

    Project skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
+import Movies from "./components/movies";
+import MovieDetails from "./components/movieDetails";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieDetails}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Route path="/" component={Movies}></Route>
+          <Redirect to="not-found"></Redirect>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/components/movies.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Movies extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Movies</h1>
+            </>
+        );
+    }
+
+}
+
+export default Movies;
+
