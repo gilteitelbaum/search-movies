d9871bde9c2f33b9b8a53916f8511499d4f6e6ee
Gil Teitelbaum <gil@nationalparalegal.edu>
2020-09-17 08:06:15 +0300

    use logger

--- src/services/restService.js
+++ src/services/restService.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import logger from "./loggerService";
 
 axios.interceptors.response.use(null, error => {
     if (!error.response || error.response.status < 400 || error.response.status >= 500) {
-        console.log("Error getting movies from server - " + error);
+        logger.error("Error getting movies from server - " + error);
         toast.error("an unexpected error happened");
     }
     return Promise.reject(error);
